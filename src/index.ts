67214822058438746e20d12831094530e3a79a15
FoLZer <nikolaenko.gleb.044@gmail.com>
2022-04-23 23:55:04 +0300

    Function arguments mapping
    Idk it just works for now, I'm fine with this

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,98 @@ function applyMapping(ast: jscodeshift.Collection<any>, mapping: {[k: string]: a
             applyMapping(func_decl, mapping.function_declarations[prev_func_name as keyof typeof mappings]);
         }
     }
+
+    if(mapping.hasOwnProperty("args")) {
+        for(const prev_arg_name of Object.keys(mapping.args)) {
+            const new_arg_name = mapping.args[prev_arg_name as keyof typeof mappings];
+            ast.find(jscodeshift.Identifier, {name: prev_arg_name}).at(0).forEach(path => {
+                const node = path.value;
+                const rootScope = path.scope;
+                const rootPath = rootScope.path;
+                jscodeshift(rootPath)
+                .find(jscodeshift.Identifier, {name: prev_arg_name})
+                .filter(function(path) { // ignore non-variables
+                    const parent = path.parent.node;
+          
+                    if (
+                        jscodeshift.types.namedTypes.MemberExpression.check(parent) &&
+                        parent.property === path.node &&
+                        !parent.computed
+                    ) {
+                        // obj.oldName
+                        return false;
+                    }
+          
+                    if (
+                        jscodeshift.types.namedTypes.Property.check(parent) &&
+                        parent.key === path.node &&
+                        !parent.computed
+                    ) {
+                        // { oldName: 3 }
+                        return false;
+                    }
+          
+                    if (
+                        jscodeshift.types.namedTypes.MethodDefinition.check(parent) &&
+                        parent.key === path.node &&
+                        !parent.computed
+                    ) {
+                        // class A { oldName() {} }
+                        return false;
+                    }
+          
+                    if (
+                        jscodeshift.types.namedTypes.ClassProperty.check(parent) &&
+                        parent.key === path.node &&
+                        !parent.computed
+                    ) {
+                        // class A { oldName = 3 }
+                        return false;
+                    }
+          
+                    if (
+                        jscodeshift.types.namedTypes.JSXAttribute.check(parent) &&
+                        //@ts-ignore
+                        parent.name === path.node &&
+                        //@ts-ignore
+                        !parent.computed
+                    ) {
+                        // <Foo oldName={oldName} />
+                        return false;
+                    }
+          
+                    return true;
+                })
+                .forEach(function(path) {
+                    let scope = path.scope;
+                    while (scope && scope !== rootScope) {
+                        if (scope.declares(prev_arg_name)) {
+                            return;
+                        }
+                        scope = scope.parent;
+                    }
+                    if (scope) { // identifier must refer to declared variable
+        
+                        // It may look like we filtered out properties,
+                        // but the filter only ignored property "keys", not "value"s
+                        // In shorthand properties, "key" and "value" both have an
+                        // Identifier with the same structure.
+                        const parent = path.parent.node;
+                        if (
+                            jscodeshift.types.namedTypes.Property.check(parent) &&
+                            parent.shorthand &&
+                            !parent.method
+                        )  {
+            
+                            path.parent.get('shorthand').replace(false);
+                        }
+        
+                        path.get('name').replace(new_arg_name);
+                    }
+                })
+            });
+        }
+    }
     
     return ast;
 }
